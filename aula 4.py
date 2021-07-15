42132bb0834aed612016cabe1218ba8b2b3ddf6a
Pedroferrersousa <pedroferrersousa@gmail.com>
2021-07-15 11:05:17 -0300

    aulas

---
+++ aula 4.py
@@ -0,0 +1,100 @@
+
+
+
+
+
+# lista = [1, 3, 5, 7]
+#
+# lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+#
+# lista_animal[0] = 'macaco'
+# print(lista_animal)
+#
+# tupla = (1, 10 ,12, 14)
+# print(len(tupla))
+# print(len(lista_animal))
+# tupla_animal = tuple(lista_animal)
+# print(type(tupla_animal))
+# print(tupla_animal)
+# lista_numerica = list(tupla)
+# print(type(lista_numerica))
+# lista_numerica[0] = 100
+# print(lista_numerica)
+
+
+
+# print(lista_animal[1])
+
+# nova_lista = lista_animal * 3
+# print(nova_lista)
+# lista.sort()
+# lista_animal.sort()
+# print(lista)
+# print(lista_animal)
+# lista_animal.reverse()
+# print(lista_animal)
+#
+
+# if 'lobo' in lista_animal:
+#     print('existe um lobo na lista')
+# else:
+#     print('não existe um lobo na lista. Será incluído')
+#     lista_animal.append('lobo')
+#     print(lista_animal)
+#
+# lista_animal.pop(0)
+# print(lista_animal)
+
+
+
+
+
+
+
+
+# a = int(input('Primeiro bimestre: '))
+# while a > 10:
+#     a = int(input('Você digitou errado. Primeiro bimestre:'))
+# b = int(input('Segundo bimestre: '))
+# while b > 10:
+#     b = int(input('Você digitou errado. Segundo bimestre:'))
+# c = int(input('Terceiro bimestre: '))
+# while c > 10:
+#     c = int(input('Você digitou errado. Terceiro bimestre:'))
+# d = int(input('Quarto bimestre: '))
+# while d > 10:
+#     d = int(input('Você digitou errado. Quarto bimestre:'))
+# media = (a + b + c + d) / 4
+# print('media: {}'.format(media))
+
+
+
+
+#
+#
+# nota = int(input('Entre com a nota:'))
+# while nota > 10:
+#     nota = int(input('Nota inválida. Entre com a nota correta:'))
+#     print(nota)
+# a = 0
+# while a <= 10:
+#     print(a)
+#     a += 1
+
+
+
+
+
+
+# a = int(input ('Entre com o número:'))
+#
+# for num in range (101):
+#     div = 0
+#     for x in range(1, num+1):
+#         resto = num % x
+#         # print(x, resto)
+#         if resto == 0:
+#             div += 1
+#
+#     if div == 2:
+#         print (num)
